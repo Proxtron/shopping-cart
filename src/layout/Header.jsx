01bc9d4a00a68f01ef056f52fef0e0b739cc346c
Proxtron <51761303+Proxtron@users.noreply.github.com>
2025-10-12 12:56:50 -0700

    Add nav selected styling and tests

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,12 +1,48 @@
 import { Link } from "react-router";
+import styles from "./Header.module.css";
+import uniqloLogo from "../assets/uniqlo-logo.svg";
+import cart from "../assets/cart.svg";
+import { useState } from "react";
 
 const Header = () => {
+    const [catalogSelected, setCatalogSelected] = useState(false);
+    const [cartSelected, setCartSelected] = useState(false);
+
+    function handleHomeClick() {
+        setCatalogSelected(false);
+        setCartSelected(false);
+    }
+
+    function handleCatalogClick() {
+        setCatalogSelected(true);
+        setCartSelected(false);
+    }
+
+    function handleCartClick() {
+        setCartSelected(true);
+        setCatalogSelected(false);
+    }
+
     return (
-        <header>
-            <nav>
-                <Link to="/">Home</Link>
-                <Link to="catalog">Catalog</Link>
-                <Link to="cart">Cart</Link>
+        <header className={styles.header}>
+            <nav className={styles.headerNav}>
+                <Link to="/" className={styles.headerLink} onClick={handleHomeClick}>
+                    <img src={uniqloLogo} alt="Back to home page" className={styles.headerLogo}/>
+                </Link>
+
+                <Link to="catalog" 
+                className={`${styles.headerLink} 
+                    ${catalogSelected && styles.underlined}
+                `} 
+                onClick={handleCatalogClick}>Catalog</Link>
+
+                <Link to="cart" 
+                className={`${styles.headerLink}
+                    ${cartSelected && styles.underlined}
+                `} 
+                onClick={handleCartClick}>
+                    <img src={cart} alt="Navigate to Cart Page"/>
+                </Link>
             </nav>
         </header>
     )
--- tests/layout/Header.test.jsx
+++ tests/layout/Header.test.jsx
@@ -1,20 +1,51 @@
 import { describe, it, expect } from "vitest"
 import { render, screen } from "@testing-library/react"
-import { createMemoryRouter, MemoryRouter, RouterProvider } from "react-router"
-import { routes } from "../../src/routes"
+import { MemoryRouter } from "react-router"
+import userEvent from "@testing-library/user-event"
 import Header from "../../src/layout/Header"
+import styles from "../../src/layout/Header.module.css"
 
 describe("Header component", () => {
-    it("renders navigation links", () => {
+    it("renders 3 navigation links", () => {
     
         render(
-        <MemoryRouter>
-            <Header/>
-        </MemoryRouter>
+            <MemoryRouter>
+                <Header/>
+            </MemoryRouter>
         );
 
-        expect(screen.getByRole("link", {name: "Home"})).toBeInTheDocument();
-        expect(screen.getByRole("link", {name: "Catalog"})).toBeInTheDocument();
-        expect(screen.getByRole("link", {name: "Cart"})).toBeInTheDocument();
+        expect(screen.getAllByRole("link").length).toBe(3);
+    });
+
+    it("shows catalog link as underlined when clicked on", async () => {
+        const user = userEvent.setup();
+
+        render(
+            <MemoryRouter>
+                <Header/>
+            </MemoryRouter>
+        );
+
+        const catalogLink = screen.getByRole("link", {name: "Catalog"});
+
+        await user.click(catalogLink);
+
+        expect(catalogLink.classList.contains(styles.underlined)).toBe(true);
+    });
+
+    it("shows cart link as underlined when clicked on", async () => {
+        const user = userEvent.setup();
+
+        render(
+            <MemoryRouter>
+                <Header/>
+            </MemoryRouter>
+        );
+
+        const cartLink = screen.getByRole("link", {name: "Navigate to Cart Page"});
+
+        await user.click(cartLink);
+
+        expect(cartLink.classList.contains(styles.underlined)).toBe(true);
     });
 })
