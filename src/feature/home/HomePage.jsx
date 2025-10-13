8c8664d789ce4b39e1a9cfc28d94d96f9df60d63
Proxtron <51761303+Proxtron@users.noreply.github.com>
2025-10-13 12:18:46 -0700

    Make a fancy home page

--- src/feature/home/HomePage.jsx
+++ src/feature/home/HomePage.jsx
@@ -1,7 +1,18 @@
+import { Link } from "react-router";
+import parka from "../../assets/pufftech-parka.jpg";
+import styles from "./HomePage.module.css";
+
 const HomePage = () => {
     return (
-        <div>
-            Hello from the home page
+        <div className={styles.homePage}>
+            <img className={styles.parkaImage} src={parka}/>
+            <div className={styles.featuredProductInfo}>
+                <h1>PUFFTECH Parka</h1>
+                <p>Everyday warmth with lightweight insulation and a water-repellant finish</p>
+                <h2 className={styles.price}>$89.90</h2>
+
+                <Link to="catalog"><button className={styles.viewButton}>View</button></Link>
+            </div>
         </div>
     );
 }
